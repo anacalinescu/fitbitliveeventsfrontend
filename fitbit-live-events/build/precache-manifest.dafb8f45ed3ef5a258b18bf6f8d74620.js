fe374c3afce232d331bbec77bd397f0b9f4177e2
Ana-Maria CĂLINESCU (87527) <ana_maria.calinescu@stud.acs.upb.ro>
2020-04-02 20:39:04 +0300

    Element meniu

---
+++ fitbit-live-events/build/precache-manifest.dafb8f45ed3ef5a258b18bf6f8d74620.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bddccaf6c5072e79ec95e10a88146ce0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a679459936ee69df49c8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "37a129d0bf31975bc039",
+    "url": "/static/js/2.f43ab3bb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f43ab3bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a679459936ee69df49c8",
+    "url": "/static/js/main.2cd7ee41.chunk.js"
+  },
+  {
+    "revision": "e528841507a2236eeb5f",
+    "url": "/static/js/runtime-main.f46db1aa.js"
+  }
+]);
